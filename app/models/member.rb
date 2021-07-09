eaffef32e6de2af5d76f5dd9057ed2e259abb80d
shin-ito0306 <shin.20000306@icloud.com>
2021-07-09 09:36:13 +0000

    [add]reration

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,4 +5,10 @@ class Member < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   attachment :member_image
+  
+  has_many :reverse_of_rerationships, class_name: 'Relationship', foreign_key: 'followed_id', dependent: :destroy
+  has_many :followers, through: :reverse_of_rerationships, sourece: :follower
+  
+  has_many :rerationships, class_name: 'Relationship', foreign_key: 'follower_id', dependent: :destory
+  has_many :followed, through: :rerationships, sourece: :followed
 end

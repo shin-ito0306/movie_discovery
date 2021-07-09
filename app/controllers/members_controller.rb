3de412734d42063d5dc18339031775e8cb4a35a1
shin-ito0306 <shin.20000306@icloud.com>
2021-07-09 07:21:20 +0000

    [add]edit.html.erb

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,4 +3,8 @@ class MembersController < ApplicationController
   def show
     @member = Member.find(current_member.id)
   end
+  
+  def edit
+    @member = Member.find(current_member.id)
+  end
 end

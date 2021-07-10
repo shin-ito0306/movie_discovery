ffea5c5dcb15c0436afb45da0efd8160133066fb
shin-ito0306 <shin.20000306@icloud.com>
2021-07-10 04:12:00 +0000

    [adjustment]API

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,4 +2,9 @@ class ReviewsController < ApplicationController
   require 'themoviedb-api'
   Tmdb::Api.key("94994320f0fad90cdb38b981b4b4af29")
   Tmdb::Api.language("ja")
+  
+  def index
+    @a = JSON.parse((Tmdb::Search.movie("Harry")).to_json)
+    @b = JSON.pretty_generate(@a)
+  end
 end

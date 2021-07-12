0b92b434ffb5f60dc272b9d3b11e982c3c334fbc
shin-ito0306 <shin.20000306@icloud.com>
2021-07-12 07:18:16 +0000

    [add]new_review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,27 @@
 class ReviewsController < ApplicationController
   
+  def index
+    @reviews = Review.where(member_id: current_member.followed_members)
+  end
+  
+  def new
+    @review = Review.new
+    @movie = Tmdb::Movie.detail(params[:movie_number])
+  end
+  
+  def create
+    @review = Review.new(review_params)
+    @review.member_id = current_member.id
+    if @review.save
+      redirect_to member_path(current_member.id)
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+  def review_params
+    params.require(:review).permit(:movie_name, :movie_image, :movie_number, :evalution, :review_detail)
+  end
 end

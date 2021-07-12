925249c593889c8f0669280661d68917e11bc831
shin-ito0306 <shin.20000306@icloud.com>
2021-07-12 11:01:05 +0000

    [add]destroy

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,7 @@
 class ReviewsController < ApplicationController
   
   def index
-    #自分の投稿が表示
+    #自分の投稿が表示されない
     @reviews = Review.where(member_id: current_member.followed_members)
   end
   
@@ -18,6 +18,25 @@ class ReviewsController < ApplicationController
     else
       render :new
     end
+  end
+  
+  def edit
+    @review = Review.find(params[:id])
+  end
+  
+  def update
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to member_path(current_member.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to member_path(current_member.id)
   end
   
   

8ae3eb562307f39ba5ec0a239164f76ad9adec35
shin-ito0306 <shin.20000306@icloud.com>
2021-07-13 08:54:38 +0000

    [add]genre

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,8 +3,9 @@ class SearchesController < ApplicationController
     if params[:kind] == "member"
       @members = Member.finder(params[:word])
     elsif params[:kind] == "movie"
-      @movies = Tmdb::Search.movie("#{params[:word]}")
-      @a = Tmdb::Genre.movie_list
+      @movies = Tmdb::Search.movie(params[:word])
+    elsif params[:kind] == "ジャンル" && params[:genre] != nil
+      @movies = Tmdb::Genre.movies(params[:genre])
     end
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def genre_search
+    @genres_all = Genre.all
+    @genres = []
+    @genre_type.each do |genre|
+      one_genre = [genre.genre_name, genre.genre_number]
+      @genre_type << one_genre
+    end
+    return @genres
+  end
 end

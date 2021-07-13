8269dc53aff6787ab62d17e2fdf88f22e0c16264
shin-ito0306 <shin.20000306@icloud.com>
2021-07-13 07:01:33 +0000

    [add]search member,keyword

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,10 @@
 class SearchesController < ApplicationController
   def search
+    if params[:kind] == "member"
+      @members = Member.finder(params[:word])
+    elsif params[:kind] == "movie"
+      @movies = Tmdb::Search.movie("#{params[:word]}")
+      @a = Tmdb::Genre.movie_list
+    end
   end
 end

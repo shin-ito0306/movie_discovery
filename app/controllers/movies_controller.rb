73b89c5c263f64c0eb8231d7c023d894afcbe49d
shin-ito0306 <shin.20000306@icloud.com>
2021-08-16 13:50:47 +0000

    [add]recommend_logic

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,5 +9,21 @@ class MoviesController < ApplicationController
   def show
     @movie = Tmdb::Movie.detail(params[:id])
     @reviews = Review.where(movie_number: params[:id])
+    movie_genre = @movie['genres'].map { |x| x['id'] }
+    standard_genre_movies = Tmdb::Genre.movies(movie_genre[0])
+    compare_movies = standard_genre_movies['results'].map { |sgm| [sgm['id'], sgm['genre_ids']] }
+    @y = []
+    k = 0
+    compare_movies.each do |compare_movie|
+      k = 0
+      movie_genre.count.times do |i|
+        if compare_movie[1].include?(movie_genre[i])
+          k += 1
+          if k == movie_genre.count
+            @y << compare_movie
+          end
+        end
+      end
+    end
   end
 end
